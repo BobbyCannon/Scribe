757e37effe95c989cf036c3faca877f9c869dc25
Bobby Cannon <bobby.cannon@live.com>
2016-04-11 20:31:33 -0400

    Updated dependencies.

--- Scribe.Data/Properties/AssemblyInfo.cs
+++ Scribe.Data/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.5937.31165")]
-[assembly: AssemblyFileVersion("2.0.5937.31165")]
+[assembly: AssemblyVersion("2.0.5945.36922")]
+[assembly: AssemblyFileVersion("2.0.5945.36922")]
