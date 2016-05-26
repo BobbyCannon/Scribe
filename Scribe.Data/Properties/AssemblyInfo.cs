bdd7b2b55127c79302c3c0509b022f12ad861ba8
Bobby Cannon <bobby.cannon@live.com>
2016-05-25 20:30:26 -0400

    Updated nuspec... again.

--- Scribe.Data/Properties/AssemblyInfo.cs
+++ Scribe.Data/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.1.5989.35640")]
-[assembly: AssemblyFileVersion("2.1.5989.35640")]
+[assembly: AssemblyVersion("2.1.5989.36893")]
+[assembly: AssemblyFileVersion("2.1.5989.36893")]
--- Scribe.Website/Properties/AssemblyInfo.cs
+++ Scribe.Website/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.1.5989.35640")]
-[assembly: AssemblyFileVersion("2.1.5989.35640")]
+[assembly: AssemblyVersion("2.1.5989.36893")]
+[assembly: AssemblyFileVersion("2.1.5989.36893")]
